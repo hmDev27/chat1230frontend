4e69b9b890287abaae8e21fb18ca7cc2847eae38
hmDev27 <hmdev27@gmail.com>
2025-08-29 00:52:45 +0630

    frontend ready for deploy

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://192.168.124.2:5001/api" : "/api",
+    baseURL: import.meta.env.MODE === "development" ? "http://192.168.1.104:5001/api" : "/api",
     withCredentials: true,
 })
