855a5b2d08f429945fe8ebe15dd7e08bfd61f188
hmDev27 <hmdev27@gmail.com>
2025-08-29 12:47:19 +0630

    Fix axios paths and backend CORS for Netlify connection

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
-import axios from 'axios'
+import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://c9093134-dfde-48fc-8111-d6c92a2aff0e-00-11l4cpgtjwwmj.sisko.replit.dev",
   withCredentials: true,
-})
+});
